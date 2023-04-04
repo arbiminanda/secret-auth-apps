05ad65cb239090d5b33a6249880200b05259a81d
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-04 10:50:52 +0700

    add login functionality

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,3 +1,22 @@
+const db = require("../models");
+const User = db.User;
+
 exports.loginPage = (req, res) => {
   res.render("login");
+};
+
+exports.userLogin = (req, res) => {
+  credentials = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  User.findOne({ email: credentials.username })
+    .then((userData) => {
+      if (userData) {
+        if (credentials.password === userData.password) {
+          res.render("secrets");
+        }
+      }
+    })
+    .catch((err) => console.log(err));
 };
--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const login = require("../controllers/login.controller");
   const r = require("express").Router();
 
-  r.get("/", login.loginPage);
+  r.route("/").get(login.loginPage).post(login.userLogin);
 
   app.use("/login", r);
 };
