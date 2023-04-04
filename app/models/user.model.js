1f9bc75c4399e030c2e0c1beca23cdff98944740
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-04 16:03:46 +0700

    use dotenv to store secret key

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,11 +1,12 @@
 const encrypt = require("mongoose-encryption");
+require("dotenv").config();
 
 module.exports = (mongoose) => {
   const userSchema = mongoose.Schema({
     email: String,
     password: String,
   });
-  const secret = "ThisIsMySecret";
+  const secret = process.env.SECRET_KEY;
   userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
   return mongoose.model("User", userSchema);
 };
