2cfd3e054c68ca9dcb2ae871cdb0176535584623
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-04 09:36:21 +0700

    add login page

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
