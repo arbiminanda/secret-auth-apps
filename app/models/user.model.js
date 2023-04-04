6d0f6e99eeb29d726b7ae064021adc2cc0a766a0
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-04 15:20:25 +0700

    use mongoose-encryption to secure password

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,7 +1,11 @@
+const encrypt = require("mongoose-encryption");
+
 module.exports = (mongoose) => {
   const userSchema = mongoose.Schema({
     email: String,
     password: String,
   });
+  const secret = "ThisIsMySecret";
+  userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
   return mongoose.model("User", userSchema);
 };
