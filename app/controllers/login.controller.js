f1a3225af976b81594afec93e306e300c5d7cbef
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-05 14:07:52 +0700

    use passport-mongoose for login

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,24 +1,33 @@
 const db = require("../models");
 const User = db.User;
 require("dotenv").config();
-const hashMethod = require(process.env.HASH_METHOD);
+const passport = require("passport");
 
 exports.loginPage = (req, res) => {
   res.render("login");
 };
 
 exports.userLogin = (req, res) => {
-  credentials = {
+  const user = new User({
     username: req.body.username,
     password: req.body.password,
-  };
-  User.findOne({ email: credentials.username })
-    .then((userData) => {
-      if (userData) {
-        if (hashMethod.compareSync(credentials.password, userData.password)) {
-          res.render("secrets");
-        }
-      }
-    })
-    .catch((err) => console.log(err));
+  });
+  req.login(user, function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("secrets");
+    });
+  });
+  // credentials = {
+  //   username: req.body.username,
+  //   password: req.body.password,
+  // };
+  // User.findOne({ email: credentials.username })
+  //   .then((userData) => {
+  //     if (userData) {
+  //       if (hashMethod.compareSync(credentials.password, userData.password)) {
+  //         res.render("secrets");
+  //       }
+  //     }
+  //   })
+  //   .catch((err) => console.log(err));
 };
